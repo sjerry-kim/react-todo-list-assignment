52b1c0a95bf31f43a12146087488be3c442e1788
jinhye <sjrry112@gmail.com>
2025-02-18 14:42:21 +0900

    Feat reset.css 추가 및 적용

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'index.css';
 import App from 'App';
+import 'reset.css';
 import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>,
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
 );
