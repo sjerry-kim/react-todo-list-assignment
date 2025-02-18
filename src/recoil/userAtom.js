49aa15a44a7400ba407fa2c6791466c67479e3a7
jinhye <sjrry112@gmail.com>
2025-02-19 01:28:23 +0900

    Feat recoil userAtom

---
+++ src/recoil/userAtom.js
@@ -0,0 +1,20 @@
+import { atom } from 'recoil';
+
+const localStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
+export const userAtom = atom({
+  key: 'userAtom',
+  default: 1,
+  effects: [localStorageEffect('user')],
+});
