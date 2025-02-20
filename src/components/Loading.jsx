89f22855d9d69817680b0d8a7c763293ce7d366a
jinhye <sjrry112@gmail.com>
2025-02-21 02:39:01 +0900

    Refactor Loading mui 적용

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,17 @@
+import { Backdrop, CircularProgress } from '@mui/material';
+
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading</h1>
-    </div>
+    <Backdrop
+      sx={(theme) => ({
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        color: 'rgba(255, 255, 255, 0.5)',
+        zIndex: theme.zIndex.drawer + 1,
+      })}
+      open={open}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>
   );
 };
 
