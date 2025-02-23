f44a1ec34572249a1c336c7849f2ca793830fb23
jinhye <sjrry112@gmail.com>
2025-02-23 12:57:05 +0900

    Style Notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from 'pages/NotFound.module.css';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { Button } from '@mui/material';
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className={styles.not_found_container}>
+      <ErrorOutlineIcon sx={{ color: '#1F4529', fontSize: '80px' }} />
+      <h1 className={styles.not_found_text}>존재하지 않는 페이지입니다.</h1>
+      <Button variant="contained" sx={{ backgroundColor: '#1F4529' }} onClick={() => navigate(-1)}>
+        뒤로가기
+      </Button>
     </div>
   );
 };
